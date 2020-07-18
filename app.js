828aa287874eadaf2548b24e58fcedcb4bdad960
Lailz <lailaabdulraheem@gmail.com>
2020-07-19 00:25:24 +0300

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cookies = require("./cookies");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
