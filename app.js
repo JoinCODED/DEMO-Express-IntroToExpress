2050611720d431208b9f95b0822d698b34697aaf
Lailz <lailaabdulraheem@gmail.com>
2021-10-10 22:25:53 +0300

    edited

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-const cookies = [
-  {
-    id: 1,
-    name: "Chocolate Chip Cookies",
-    slug: "chocolate-chip-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 15,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg",
-  },
-  {
-    id: 2,
-    name: "Peanut Butter Cookies",
-    slug: "peanut-butter-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 3,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/dcd4f799-7353-4e56-ba50-623581cba3bc.jpg",
-  },
-  {
-    id: 3,
-    name: "Salted Caramel Cookies",
-    slug: "salted-caramel-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 10,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/586da0ed-8a79-4390-9137-f60852ca312a.jpg",
-  },
-];
+const data = require("./data");
 
 const app = express();
 app.use(cors());
@@ -38,10 +9,12 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.get("/cookies", (req, res) => {
-  res.json(cookies);
+app.get("/facts", (req, res) => {
+  res.json(data);
 });
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${8000}`);
 });
---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    id: 1,
+    name: "Chocolate Chip Cookies",
+  },
+  {
+    id: 2,
+    name: "Peanut Butter Cookies",
+  },
+  {
+    id: 3,
+    name: "Salted Caramel Cookies",
+  },
+];
+
+module.exports = data;
